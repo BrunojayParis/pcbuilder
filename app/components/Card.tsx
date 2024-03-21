102cc1a4cf5d7eb1ec0d431bfbba59fb4e90c226
Bruno <brunojparisz@gmail.com>
2024-03-21 16:36:25 -0300

    Link in options

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,24 +1,30 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 interface CardProps {
-title: string;
-subtitle: string;
-photo: StaticImageData;
-bgColor?: string;
-
+  title: string;
+  subtitle: string;
+  photo: StaticImageData;
+  bgColor?: string;
+  href: string;
 }
 
+const Card: FC<CardProps> = ({ title, subtitle, photo, href }) => {
+  return (
+    <Link
+      href={href}
+      className="flex flex-col items-center justify-center p-10 w-full bg-slate-500 rounded-lg shadow-lg text-black hover:bg-black hover:text-white"
+    >
+      <h2 className="text-2xl font-bold mb-auto">{title}</h2>
+      <p className="text-lg font-medium mt-4">{subtitle}</p>
+      <Image
+        src={photo}
+        alt="javelin"
+        className="object-cover h-auto mt-auto"
+      />
+    </Link>
+  );
+};
 
-const Card : FC<CardProps> =({title, subtitle, photo})=> {
-    return (
-        <div className= "flex flex-col items-center justify-center p-10 w-full bg-slate-500 rounded-lg shadow-lg text-black hover:bg-black hover:text-white">
-            <h2 className="text-2xl font-bold mb-auto">{title}</h2>
-            <p className="text-lg font-medium mt-4">{subtitle}</p>
-            <Image src={photo} alt="javelin" className="object-cover h-auto mt-auto" />
-        </div>
-    );
-  };
-  
-  export default Card;
-  
+export default Card;
--- app/components/Options.tsx
+++ app/components/Options.tsx
@@ -16,16 +16,19 @@ const Options: FC<OptionsProps> = ({javelin, hellfire, tomahawk}) => {
       <h2 className="text-4xl font-bold text-white">Ready. Set. Game</h2>
       <div className="flex flex-col gap-10 mx-10 mt-12 md:flex-row ">
         <Card
+          href="/Mini1"
           title="JAVELIN"
           subtitle="Se arranca fuerte. La mejor combinación de procesadores de 6 núcleos con NVIDIA® RTX™ 30 series y el nuevo gabinete Q300L V2."
           photo={javelin}
         />
         <Card
+          href="/Mini2"
           title="HELLFIRE"
           subtitle="No andamos con vueltas con Hellfire. Procesadores de 12 núcleos con NVIDIA® RTX™ que pueden con todo en 1440p."
           photo={hellfire}
         />
         <Card
+          href="/Mini3"
           title="TOMAHAWK"
           subtitle="Lo mejor de lo mejor. Procesadores de infinitos nucleos y GPU NVIDIA® RTX™ en el gabinete mas demandado, el NR200."
           photo={tomahawk}
